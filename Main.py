f416414c111f9d2a75dfcb1dbc24cc87d1c284ad
URK21CS1079 <110010648+URK21CS1079@users.noreply.github.com>
2022-09-18 21:20:23 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,40 +4,46 @@ class Node:
     self.next = None
 
 
-class Stack:
+class Queue:
   def __init__(self):
     self.head = None
+    self.last = None
 
-  def push(self, data) -> None:
+  def enqueue(self, data) -> None:
+      last = self.last
       node = Node(data)
-      node.next = self.head
-      self.head = node
+      if(self.last is None and self.head is None):
+        self.last , self.head = node , node
+      else:
+        self.last.next = node
+        self.last = node
 
-  def pop(self) -> None:
-      if(self.head is not None):
-          self.head = self.head.next
-
-  def status(self):
-    ptr = self.head
-    if(self.head is None):
-        print("None")
+  def dequeue(self) -> None:
+    if self.head == self.last:
+        self.head = self.last = None
     else:
-        while (ptr):
-            print(ptr.data, end="=>")
-            ptr = ptr.next
-        print("None")
+        self.head = self.head.next
+
+    
+
+  def status(self) -> None:
+    ptr = self.head
+    while ptr != None:
+      print(ptr.data,end = "=>")
+      ptr = ptr.next
+    print("None")
 
 
 # Do not change the following code
-stack = Stack()
+queue = Queue()
 operations = []
 for specific_operation in input().split(','):
     operations.append(specific_operation.strip())
 input_data = input()
 data = input_data.split(',')
 for i in range(len(operations)):
-  if operations[i] == "push":
-    stack.push(int(data[i]))
-  elif operations[i] == "pop":
-    stack.pop()
-stack.status()
+  if operations[i] == "enqueue":
+    queue.enqueue(int(data[i]))
+  elif operations[i] == "dequeue":
+    queue.dequeue()
+queue.status()
